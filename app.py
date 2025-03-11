35db6b8bd8c5dbb20768bbde1ad45599ae327dd3
Coco <corentinpineau25@gmail.com>
2025-03-11 11:22:32 +0100

    Test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import app_routes  # Importez les routes depuis routes.py
+import models  # Importez les fonctions de models.py
 
 # Créez l'application Flask
 app = Flask(__name__)
@@ -9,4 +10,7 @@ app.register_blueprint(app_routes)
 
 
 if __name__ == '__main__':
+    print(models.get_mongo_client())  # Testez la connexion MongoDB
+    print(models.get_data())  # Testez la récupération de données
     app.run(debug=True)  # Activez le mode debug pour le développement
+    
