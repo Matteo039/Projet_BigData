df5b5660fbd438cb495abef0f0fddd9b28818f7c
Coco <corentinpineau25@gmail.com>
2025-04-01 10:07:45 +0200

    fusion partie 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import app_routes  # Importez les routes depuis routes.py
+import models  # Importez models.py pour les fonctions de traitement de données
 
 # Créez l'application Flask
 app = Flask(__name__)
@@ -9,4 +10,7 @@ app.register_blueprint(app_routes)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Activez le mode debug pour le développement
+    # Vérifiez si la base de données est accessible
+    client = models.get_mongo_client()
+    
+    #app.run(debug=True)  # Activez le mode debug pour le développement
