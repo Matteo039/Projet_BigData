883e85bdf586a42cd905cfcd006ad392fa8b62c3
Matteo039 <matteo.aillet@reseau.eseo.fr>
2025-04-02 11:33:57 +0200

    Normalement c'est bien

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from routes import app_routes  # Importez les routes depuis routes.py
-import models  # Importez models.py pour les fonctions de traitement de données
+from routes import app_routes
+import models
 
-# Créez l'application Flask
 app = Flask(__name__)
-
-# Enregistrez les routes
 app.register_blueprint(app_routes)
 
-
 if __name__ == '__main__':
-    # Vérifiez si la base de données est accessible
-    models.get_mongo_client()  # Connexion à MongoDB
-    print(models.get_collections())  # Récupère les collections
-    print(models.get_data_from_collection('villes'))  # Récupère les données d'une collection
-    #app.run(debug=True)  # Activez le mode debug pour le développement
+    models.get_mongo_client()
+    #print(models.get_collections())
+    #print(models.get_data_from_collection('villes'))
+    app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,52 +1,32 @@
 from flask import Blueprint, jsonify, request, render_template
-# Si vous utilisez models.py:
-from models import get_data, insert_data, process_data  # Importez les fonctions
+from models import get_data, get_available_cities
 
-# Créez un Blueprint pour organiser les routes
 app_routes = Blueprint('app_routes', __name__)
 
 @app_routes.route('/')
 def index():
-    """Route principale (page d'accueil)."""
-    return render_template('index.html')  # Si vous avez un template
-    # return "Bienvenue dans votre application de traitement de données!"
+    return render_template('index.html')
 
 @app_routes.route('/city', methods=['GET'])
 def cities():
-    """Route pour chaque ville."""
-    return render_template('city.html')
+    available_cities = get_available_cities()
+    selected_ville = request.args.get('ville')
+    data = get_data(ville=selected_ville)  # Récupérer les données
+
+    # Ajoute ces lignes pour le débogage
+    for item in data:
+        print("Item:", item)  # Affiche l'item actuel pour voir sa structure
+
+    return render_template('city.html', cities=available_cities, ville=selected_ville, data=data)
 
 @app_routes.route('/about')
 def a_propos():
-    """Route pour chaque ville."""
     return render_template('aPropos.html')
 
 @app_routes.route('/data', methods=['GET'])
 def get_all_data_route():
-    """Récupère toutes les données."""
     try:
         data = get_data()
         return jsonify(data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@app_routes.route('/data', methods=['POST'])
-def add_data_route():
-    """Ajoute des données."""
-    try:
-        data = request.get_json()
-        insert_data(data)
-        return jsonify({'message': 'Données ajoutées avec succès'}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
-
-@app_routes.route('/process', methods=['POST'])
-def process_data_route():
-    """Route pour le traitement des données."""
-    try:
-        input_data = request.get_json()  # ou request.form, si vous envoyez des données de formulaire
-        results = process_data(input_data) # Appel de votre fonction de models.py
-        return jsonify(results)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
