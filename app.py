53fbccbca4fde10798f3c5182367227a2b49228d
Matteo039 <matteo.aillet@reseau.eseo.fr>
2025-04-05 14:25:55 +0200

    c'est corrigé (normalement comme la derniere fois)

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ app.register_blueprint(app_routes)
 
 if __name__ == '__main__':
     models.get_mongo_client()
-
-    #print(models.get_collections())
-    #print(models.get_data_from_collection('villes'))
-
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -11,30 +11,8 @@ def index():
 def cities():
     available_cities = get_available_cities()
     selected_ville = request.args.get('ville')
-
-    data = get_data(ville=selected_ville)  # Récupérer les données
-
-    # Ajoute ces lignes pour le débogage
-    for item in data:
-        print("Item:", item)  # Affiche l'item actuel pour voir sa structure
-
-    return render_template('city.html', cities=available_cities, ville=selected_ville, data=data)
-
-@app_routes.route('/about')
-def a_propos():
-    return render_template('aPropos.html')
-
-@app_routes.route('/data', methods=['GET'])
-def get_all_data_route():
-    try:
-        data = get_data()
-        return jsonify(data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
     selected_date = request.args.get('date')  # Récupérer la date
 
     graph_data = get_data(ville=selected_ville, date=selected_date)  # Passer la date
 
     return render_template('city.html', cities=available_cities, ville=selected_ville, graph_data=graph_data, selected_date=selected_date)
-
